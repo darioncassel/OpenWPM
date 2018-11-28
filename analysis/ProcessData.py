96c5fc2e310c81880d525eddc499872ad6ed2e2a
Darion Cassel <darion.cassel@gmail.com>
2018-11-28 13:39:51 -0500

    Update Experiment to perform in blocks, update Analysis to perform permutation test, add ProcessData for extracting features

---
+++ analysis/ProcessData.py
@@ -0,0 +1,86 @@
+import os
+
+class ProcessData():
+
+    def __init__(self, sources_dir, feature_extract):
+        self.sources_dir = sources_dir
+        self.feature_extract = feature_extract
+
+    def read_files(self):
+        experimental_lines = []
+        control_lines = []
+        for filename in os.listdir(self.sources_dir):
+            with open(self.sources_dir + filename, 'r') as data_file:
+                if "control" in filename:
+                    control_lines.append(data_file.readlines())
+                else:
+                    experimental_lines.append(data_file.readlines())
+        return experimental_lines, control_lines
+    
+    def extract_features(self, experimental_observations, control_observations):
+        processed_exp = []
+        processed_con = []
+        for l in experimental_observations:
+            processed_exp.append(self.feature_extract(l))
+        for l in control_observations:
+            processed_con.append(self.feature_extract(l))
+        return processed_exp, processed_con
+    
+    def generate_feature_matrix(self, features_e, features_c):
+        all_topics = []
+        for tl in features_e:
+            for t in tl:
+                if t not in all_topics:
+                    all_topics.append(t)
+        for tl in features_c:
+            for t in tl:
+                if t not in all_topics:
+                    all_topics.append(t)
+        exp_matrix = []
+        for tl in features_e:
+            vec = [0] * len(all_topics)
+            for t in tl:
+                i = all_topics.index(t)
+                vec[i] = 1
+            exp_matrix.append(vec)
+        con_matrix = []
+        for tl in features_c:
+            vec = [0] * len(all_topics)
+            for t in tl:
+                i = all_topics.index(t)
+                vec[i] = 1
+            con_matrix.append(vec)
+        return all_topics, exp_matrix, con_matrix
+    
+    def generate_total_feature_matrix(self, features_e, features_c):
+        all_topics = []
+        for tl in features_e:
+            for t in tl:
+                if t not in all_topics:
+                    all_topics.append(t)
+        for tl in features_c:
+            for t in tl:
+                if t not in all_topics:
+                    all_topics.append(t)
+        total_matrix = []
+        for tl in features_e:
+            vec = [0] * len(all_topics)
+            for t in tl:
+                i = all_topics.index(t)
+                vec[i] = 1
+            total_matrix.append(vec)
+        for tl in features_c:
+            vec = [0] * len(all_topics)
+            for t in tl:
+                i = all_topics.index(t)
+                vec[i] = 1
+            total_matrix.append(vec)
+        return total_matrix
+    
+    def process(self):
+        experimental_observations, control_observations = self.read_files()
+        features_e, features_c = self.extract_features(experimental_observations, control_observations)
+        self.feature_matrix = self.generate_total_feature_matrix(features_e, features_c)
+    
+    def save_data(self):
+        return self.feature_matrix
--- experiment/Experiment.py
+++ experiment/Experiment.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from six.moves import range
 from automation import CommandSequence, TaskManager
+from analysis.ProcessData import ProcessData
 from copy import copy
 from sys import argv
 from random import shuffle
@@ -15,11 +16,16 @@ class Stage(object):
 
 
 class Experiment(object):
-    def __init__(self, num_browsers, data_directory="~/Desktop/", stages=[]):
+    def __init__(self, num_browsers=10, num_blocks=1, feature_extract=None, data_directory="~/Desktop/", stages=[]):
         self.num_browsers = num_browsers
+        self.num_blocks = num_blocks
         self.stages = stages
+        self.feature_extract = feature_extract
         self.data_directory = data_directory
+        self.blocked_data = []
+        self.block_assignments = []
         self.manager = self.init_manager()
+        self.pd = ProcessData(self.data_directory + "sources/", self.feature_extract)   
 
     def add_stage(self, name, group, site, action):
         self.stages.append(Stage(name, group, site, action))
@@ -45,6 +51,7 @@ class Experiment(object):
         # Loads the manager preference and NUM_BROWSERS copies of the default browser dictionaries
         manager_params, browser_params = TaskManager.load_default_params(self.num_browsers)
         assignments = self.randomized_assignments()
+        self.block_assignments.append(assignments)
 
         # Update browser configuration (use this for per-browser settings)
         for i in range(self.num_browsers):
@@ -64,7 +71,7 @@ class Experiment(object):
         manager = TaskManager.TaskManager(manager_params, browser_params)
         return manager
     
-    def run(self):
+    def run_once(self):
         for stage in self.stages:
             command_sequence = CommandSequence.CommandSequence(stage.site)
             if isinstance(stage.actions, list):
@@ -77,3 +84,24 @@ class Experiment(object):
             else:
                 self.manager.execute_command_sequence(command_sequence, index='**')
         self.manager.close()
+    
+    def run(self):
+        self.blocked_data = []
+        for _ in range(self.num_blocks):
+            self.init_manager()
+            self.run_once()
+            self.pd.process()
+            self.blocked_data.append(self.pd.save_data())
+        
+    def get_observations(self):
+        return self.blocked_data
+    
+    def get_assignments(self):
+        assignments = []
+        for _ in range(self.num_blocks):
+            assignment = [0] * self.num_blocks
+            for i in range(self.num_blocks/2):
+                assignment[i] = 1
+            assignments.append(assignment)
+        # return self.block_assignments
+        return assignments
