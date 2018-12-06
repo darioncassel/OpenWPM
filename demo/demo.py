b230fbaa46620781fee861296e82ec06f3a06e07
Darion Cassel <darion.cassel@gmail.com>
2018-12-05 19:23:52 -0500

    Add demo experiment

---
+++ demo/demo.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import
+from demo_util import *
+from sys import argv
+
+
+NUM_BROWSERS = 2
+NUM_BLOCKS = 2
+TEST_NAME = "demo"
+
+
+def run_experiment(data_directory, num_browsers=20, num_blocks=10):
+    persona = Experiment(
+        data_directory=data_directory,
+        num_browsers=num_browsers,
+        num_blocks=num_blocks,
+        feature_extract=extract_topics,
+        save_path=TEST_NAME + "_data.txt"
+    )
+    persona.add_stage(
+        "start", "all", "https://www.youtube.com/",
+        [visit, scroll]
+    )
+    persona.add_stage(
+        "treatment", "experimental", "https://www.google.com/", 
+        [visit]
+    )
+    persona.add_stage(
+        "measurement", "all", "https://www.youtube.com/", 
+        [visit, scroll, save_page_source]
+    )
+    persona.run()
+    persona.save_data()
+    return persona.get_observations(), persona.get_assignments()
+
+def run_analysis(observations, assignments):
+    analysis = Analysis(
+        observed_values=observations,
+        unit_assignments=assignments,
+        test_statistic=test_statistic,
+        save_path=TEST_NAME + "_results.txt"
+    )
+    analysis.perform()
+    analysis.save_results()
+    return analysis.get_results()
+
+def main():
+    data_directory = "/home/vagrant/Desktop/"
+    observations, assignments = run_experiment(data_directory, NUM_BROWSERS, NUM_BLOCKS)
+    results = run_analysis(observations, assignments)
+    print "p-value: %f" % results
+
+
+if __name__ == "__main__":
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+        from experiment.Experiment import Experiment, Stage
+        from analysis.Analysis import Analysis
+    else:
+        from ..experiment.Experiment import Experiment, Stage
+        from ..analysis.Analysis import Analysis
+    main()
---
+++ demo/demo_util.py
@@ -0,0 +1,44 @@
+
+def visit(command_sequence):
+    command_sequence.get(sleep=0, timeout=60)
+
+def scroll(command_sequence):
+    command_sequence.page_down(count=5)
+
+def save_page_source(command_sequence):
+    command_sequence.dump_page_source(suffix='__BY_GROUP', timeout=120)
+
+def extract_topics(lines):
+    topics = []
+    for l in lines:
+        if "- Topic" in l:
+            topic_str = l.split("- Topic")[0][-20:]
+            if ">" in topic_str:
+                topic = topic_str.split(">")[-1]
+                topics.append(topic.strip())
+            elif "\"" in topic_str:
+                topic = topic_str.split("\"")[-1]
+                topics.append(topic.strip())
+    return list(set(topics))
+
+def normalized_difference(m1, m2):
+    num_rows = min(len(m1), len(m2))
+    num_cols = min(len(m1[0]), len(m2[0]))
+    diff = 0.0
+    for i in range(num_rows):
+        for j in range(num_cols):
+            diff += abs(m1[i][j] - m2[i][j])
+    return diff / (num_rows * num_cols)
+
+def test_statistic(observed_values, unit_assignments):
+    norm_diff = 0
+    for block_num, block in enumerate(observed_values):
+        m1 = []
+        m2 = []
+        for browser_id, data in enumerate(block):
+            if unit_assignments[block_num][browser_id]:
+                m1.append(data)
+            else:
+                m2.append(data)
+        norm_diff += normalized_difference(m1, m2)
+    return norm_diff
